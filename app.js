bbfac84d0478b9ba724f5ac04a83a85e3be93169
Marc <marc@haupt.digital>
2020-03-23 14:45:44 +0100

    Add read file function (#2)

--- app.js
+++ app.js
@@ -1,5 +1,25 @@
-function sayHi() {
-  console.log('Hello from the crypt');
+const fs = require('fs');
+const [command, key, value] = process.argv.slice(2);
+
+function get() {
+  try {
+    const data = fs.readFileSync('./db.json', 'utf8');
+    const db = JSON.parse(data);
+    console.log(db[key]);
+    return db[key];
+  } catch (err) {
+    console.error(err);
+  }
 }
 
-sayHi();
+function set() {
+  console.log('called SET', key, value);
+}
+
+if (command === 'get') {
+  get();
+} else if (command === 'set') {
+  set();
+} else {
+  console.error('unknown command');
+}
