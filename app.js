41c563ad8a482cf3195502ae95100fd5873c319c
hauptdigital <marc@haupt.digital>
2020-03-23 16:03:45 +0100

    Add unset function

--- app.js
+++ app.js
@@ -15,23 +15,40 @@ function set() {
   try {
     const db = get();
     db[key] = value;
-    const data = fs.writeFileSync('./db.json', JSON.stringify(db, null, 2));
-    console.log(data);
+    fs.writeFileSync('./db.json', JSON.stringify(db, null, 2));
+    debugSkeleton();
+    console.log('called SET with', key, value);
+    console.log('New', key, 'is', value);
   } catch (error) {
     console.error(error);
   }
+}
+
+function unset() {
+  try {
+    const db = get();
+    delete db[key];
+    fs.writeFileSync('./db.json', JSON.stringify(db, null, 2));
+    debugSkeleton();
+    console.log('called UNSET with', key, value);
+    console.log(key, 'was deleted');
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+function debugSkeleton() {
   console.log(
     '\nline      .-.\nline     (o.o)\nline      |=|\nline     __|__\nline   //.=|=.\\\nline  // .=|=. \\\nline  \\ .=|=. // \nline   \\(_=_)// \nline    (:| |:)\nline     || ||\nline     () ()\nline     || ||\nline     || ||\nline    ==   ==\nline'
   );
-
-  console.log('called SET with', key, value);
-  console.log('New', key, 'is', value);
 }
 
 if (command === 'get') {
   get();
 } else if (command === 'set') {
   set();
+} else if (command === 'unset') {
+  unset();
 } else {
   console.error('unknown command');
 }
