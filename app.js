3deb3bda65a3c00d4a365fdb80ebc08d65ca6ffe
Marc <marc@haupt.digital>
2020-03-24 21:44:45 +0100

    Verify master password (#10)
    * Correct function names
    
    * Add ask master password question

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const { get, set, unset } = require('./lib/commands');
 const [command, key] = process.argv.slice(2);
-const { askForPassword } = require('./lib/questions');
+const { askForPassword, askForMasterPassword } = require('./lib/questions');
+const { readMasterPassword } = require('./lib/passwords');
 
 async function run() {
+  const dbMasterPassword = await readMasterPassword();
+  const userMasterPassword = await askForMasterPassword();
+  if (userMasterPassword != dbMasterPassword) {
+    console.log('You are not allowed to enter the password crypt');
+    return;
+  }
   if (command === 'get') {
     get(key);
   } else if (command === 'set') {
