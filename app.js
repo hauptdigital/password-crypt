bb63479d49a26a9bb4cf4ef90aaf89e8debd3ed5
hauptdigital <marc@haupt.digital>
2020-03-24 22:26:05 +0100

    Add master password check using hashed database entries

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const { get, set, unset, reset } = require('./lib/commands');
 const [command, key] = process.argv.slice(2);
 const { askForPassword, askForMasterPassword } = require('./lib/questions');
 const { readMasterPassword } = require('./lib/database');
+const { verifyHash } = require('./lib/crypto');
 
 async function run() {
   if (command === 'reset') {
@@ -9,9 +10,14 @@ async function run() {
     reset(newMasterPassword);
     return;
   }
+
   const dbMasterPassword = await readMasterPassword();
   const userMasterPassword = await askForMasterPassword();
-  if (userMasterPassword != dbMasterPassword) {
+  const userMasterPasswordIsCorrect = verifyHash(
+    userMasterPassword,
+    dbMasterPassword
+  );
+  if (!userMasterPasswordIsCorrect) {
     console.log('††† You are not allowed to enter the password crypt †††');
     return;
   }
