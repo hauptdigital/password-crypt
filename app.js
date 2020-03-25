256fbda05853fd0e55ec17f38b3aa5d0220234c5
Marc <marc@haupt.digital>
2020-03-25 09:41:44 +0100

    Reset (#11)
    * Add read file function
    
    * Add reset master password function
    
    * Add master password check using hashed database entries

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
-const { get, set, unset } = require('./lib/commands');
+const { get, set, unset, reset } = require('./lib/commands');
 const [command, key] = process.argv.slice(2);
 const { askForPassword, askForMasterPassword } = require('./lib/questions');
-const { readMasterPassword } = require('./lib/passwords');
+const { readMasterPassword } = require('./lib/database');
+const { verifyHash } = require('./lib/crypto');
 
 async function run() {
+  if (command === 'reset') {
+    const newMasterPassword = await askForMasterPassword();
+    reset(newMasterPassword);
+    return;
+  }
+
   const dbMasterPassword = await readMasterPassword();
   const userMasterPassword = await askForMasterPassword();
-  if (userMasterPassword != dbMasterPassword) {
-    console.log('You are not allowed to enter the password crypt');
+  const userMasterPasswordIsCorrect = verifyHash(
+    userMasterPassword,
+    dbMasterPassword
+  );
+  if (!userMasterPasswordIsCorrect) {
+    console.log('††† You are not allowed to enter the password crypt †††');
     return;
   }
   if (command === 'get') {
