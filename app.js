1c46b38b5b2855c67552b8a4132d8e17a50ec12d
hauptdigital <marc@haupt.digital>
2020-03-25 16:19:08 +0100

    Add connect and disconnect function

--- app.js
+++ app.js
@@ -1,40 +1,45 @@
 const { get, set, unset, reset } = require('./lib/commands');
 const [command, key] = process.argv.slice(2);
 const { askForPassword, askForMasterPassword } = require('./lib/questions');
-const { readMasterPassword, connectToMongoDB } = require('./lib/database');
+const { readMasterPassword } = require('./lib/database');
+const { connectToDB, closeConnectionToDB } = require('./lib/db');
 const { verifyHash } = require('./lib/crypto');
 
 async function run() {
-  if (command === 'connect') {
-    connectToMongoDB();
-  }
-  /*
-  if (command === 'reset') {
-    const newMasterPassword = await askForMasterPassword();
-    reset(newMasterPassword);
-    return;
-  }
+  try {
+    await connectToDB();
 
-  const dbMasterPassword = await readMasterPassword();
-  const userMasterPassword = await askForMasterPassword();
-  const userMasterPasswordIsCorrect = verifyHash(
-    userMasterPassword,
-    dbMasterPassword
-  );
-  if (!userMasterPasswordIsCorrect) {
-    console.log('††† You are not allowed to enter the password crypt †††');
-    return;
+    if (command === 'reset') {
+      const newMasterPassword = await askForMasterPassword();
+      reset(newMasterPassword);
+      return;
+    }
+
+    const dbMasterPassword = await readMasterPassword();
+    const userMasterPassword = await askForMasterPassword();
+    const userMasterPasswordIsCorrect = verifyHash(
+      userMasterPassword,
+      dbMasterPassword
+    );
+    if (!userMasterPasswordIsCorrect) {
+      console.log('††† You are not allowed to enter the password crypt †††');
+      return;
+    }
+    if (command === 'get') {
+      get(key);
+    } else if (command === 'set') {
+      const password = await askForPassword(key);
+      set(key, password);
+    } else if (command === 'unset') {
+      unset(key);
+    } else {
+      console.log('Unknown command');
+    }
+  } catch (error) {
+    console.error(error);
+  } finally {
+    await closeConnectionToDB();
   }
-  if (command === 'get') {
-    get(key);
-  } else if (command === 'set') {
-    const password = await askForPassword(key);
-    set(key, password);
-  } else if (command === 'unset') {
-    unset(key);
-  } else {
-    console.log('Unknown command');
-  }*/
 }
 
 run();
